39bd2ff6de6b9ac8b3c0cefe35d19eabffb6b019
Chirag Jain <cjain9975@gmail.com>
2026-03-16 20:59:31 +0530

    IRTC Backend System

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -132,6 +132,11 @@ async def get_current_user_details_from_access_token(
     user_id = payload.get("sub")
     jti = payload.get("jti")
     user_id_from_access_token_cache = await cache_get(key=f"cache:auth:access:jti:{jti}")
+    if user_id_from_access_token_cache == None:
+        raise BaseAppException(
+            messages=["Invalid access token. Access token is not available into cache"],
+            status_code=401,
+        )
     if int(user_id) != int(user_id_from_access_token_cache):
         raise BaseAppException(
             messages=["Access-Token (User-ID) is not matched with stored cache"],
