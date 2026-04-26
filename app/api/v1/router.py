0a4cdfb5ee2145b1164a7d89be18a0afb52f1567
Chirag Jain <cjain9975@gmail.com>
2026-04-26 16:59:29 +0530

    IRTC Backend System

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -6,6 +6,8 @@ API v1 Router
 from fastapi import APIRouter
 from app.api.v1.users import router as users_router
 from app.api.v1.auth import router as auth_router
+from app.api.v1.admin_master_data import router as admin_master_data_router
+
 
 router = APIRouter()
 
@@ -19,4 +21,10 @@ router.include_router(
     auth_router,
     prefix="/auth",
     tags=["Users"],
+)
+
+router.include_router(
+    admin_master_data_router,
+    prefix="/admin/master-data",
+    tags=["Admin Master Data"],
 )
