02b6f243d6b176c62903afb6ee0b48721c7d0aa4
Chirag Jain <cjain9975@gmail.com>
2026-04-26 12:20:58 +0530

    IRTC Backend System

---
+++ app/domains/master_data/route.py
@@ -0,0 +1,64 @@
+
+from datetime import datetime
+from fastapi.datastructures import Default
+from sqlalchemy import (
+    String,
+    DateTime,
+    Integer,
+    UniqueConstraint,
+    Index,
+    Enum
+)
+from sqlalchemy.orm import Mapped, mapped_column
+from app.infrastructure.database.base import Base
+from app.common.utils.datetime import now_ist
+
+
+# =========================================================
+# ENUMS
+# =========================================================
+
+status_enum = Enum(
+    "A",
+    "Z",
+    name="status_enum",
+)
+
+
+class Route(Base):
+
+    """
+    Route table
+    """
+
+    __tablename__ = "Route"
+    
+    __table_args__ = (
+        UniqueConstraint("train_id", name="uq_train_id"),
+        Index("ix_train_status", "status"),
+    )
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True,
+        nullable=False,
+    )
+    # this train_id is the primary key of Train table model only
+    # but i don't want to treat as foreign-key concept
+    train_id: Mapped[int] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=now_ist(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=now_ist(),
+        onupdate=now_ist(),
+        nullable=False,
+    )
+    status: Mapped[str] = mapped_column(
+        status_enum,
+        nullable=False,
+        default="A",
+    )
