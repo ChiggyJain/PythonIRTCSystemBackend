9f603dc1dda20060f7cb8552297d7c0e36122adb
Chirag Jain <cjain9975@gmail.com>
2026-04-26 15:24:41 +0530

    IRTC Backend System

--- app/infrastructure/database/models_importer.py
+++ app/infrastructure/database/models_importer.py
@@ -18,5 +18,9 @@ Only import models.
 from app.domains.users import users_model  # noqa
 from app.domains.auth import usertokens_model  # noqa
 from app.domains.security import models # noqa
+from app.domains.master_data import (
+    stations_model, trains_model, seats_model,
+    routes_model, routestations_model, schedules_model
+) # noqa
 
 
