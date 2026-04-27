728ee8b33cbd2097882e40e15e7c08b17f41c81d
Chirag Jain <cjain9975@gmail.com>
2026-04-27 08:23:41 +0530

    IRTC Backend System

--- app/dependencies/master_data.py
+++ app/dependencies/master_data.py
@@ -1,6 +1,7 @@
 
 from fastapi import Depends
 from app.domains.master_data.stations_service import StationsService
+from app.domains.master_data.trains_service import TrainsService
 from app.domains.master_data.repository.base import MasterDataRepositoryBase
 from app.dependencies.repositories import get_master_data_repository
 
@@ -9,3 +10,9 @@ def get_stations_service(
     repo: MasterDataRepositoryBase = Depends(get_master_data_repository),
 ) -> StationsService:
     return StationsService(repo)
+
+
+def get_trains_service(
+    repo: MasterDataRepositoryBase = Depends(get_master_data_repository),
+) -> TrainsService:
+    return TrainsService(repo)
