a066341345eaa394565dc092e46a113428ecff72
Chirag Jain <cjain9975@gmail.com>
2026-04-27 08:14:28 +0530

    IRTC Backend System

--- app/domains/master_data/repository/base.py
+++ app/domains/master_data/repository/base.py
@@ -2,6 +2,8 @@
 from abc import ABC, abstractmethod
 from typing import Any
 from app.domains.master_data.stations_model import Stations
+from app.domains.master_data.trains_model import Trains
+from app.domains.master_data.seats_model import Seats
 
 
 class MasterDataRepositoryBase(ABC):
@@ -17,6 +19,46 @@ class MasterDataRepositoryBase(ABC):
         status: str = "A",
     ) -> Stations:
         pass
+    
+    
+    @abstractmethod
+    async def create_train(
+        self,
+        *,
+        train_number: str,
+        train_name: str,
+        coach_name: str,
+        total_seats: int,
+        status: str = "A",
+    ) -> Trains:
+        """
+        Example:
+        create_train(
+            train_number="TRAIN1233",
+            train_name="T1",
+            coach_name="AC",
+            total_seats=20,
+        )
+        """
+        pass
+    
+    @abstractmethod
+    async def create_seats(
+        self,
+        *,
+        train_id: int,
+        seat_details: list[dict[str, Any]],
+        status: str = "A",
+    ) -> list[Seats]:
+        """
+        Example seat_details:
+        [
+            {"seat_number": 1, "seat_type": "LOWER", "price": 500},
+            {"seat_number": 2, "seat_type": "UPPER", "price": 300},
+        ]
+        """
+        pass
+
 
     @abstractmethod
     async def add_outbox_event(
@@ -29,6 +71,7 @@ class MasterDataRepositoryBase(ABC):
         status: str,
     ) -> None:
         pass
+
 
     @abstractmethod
     async def commit(self) -> None:
