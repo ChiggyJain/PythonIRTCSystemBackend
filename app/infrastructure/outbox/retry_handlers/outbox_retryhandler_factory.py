a1cdf55a46e53ab9f1fcfd0216b2842ef3156443
Chirag Jain <cjain9975@gmail.com>
2026-04-30 12:11:41 +0530

    IRTC Backend System

--- app/infrastructure/outbox/retry_handlers/outbox_retryhandler_factory.py
+++ app/infrastructure/outbox/retry_handlers/outbox_retryhandler_factory.py
@@ -1,6 +1,7 @@
 
 
 from app.infrastructure.outbox.retry_handlers.emailchanged_otp_outbox_retryhandler import EmailChangedOtpOutboxRetryHandler
+from app.infrastructure.outbox.retry_handlers.emailverification_otp_outbox_retryhandler import EmailVerificationOtpOutboxRetryHandler
 
 
 class OutboxRetryHandlerFactory:
@@ -10,3 +11,5 @@ class OutboxRetryHandlerFactory:
         retry_handler_type =  kwargs.get("retry_handler_type", None)
         if retry_handler_type == "EMAILCHANGED_OTP":
             return EmailChangedOtpOutboxRetryHandler(**kwargs)
+        if retry_handler_type == "EMAILVERIFICATION_OTP":
+            return EmailVerificationOtpOutboxRetryHandler(**kwargs)
