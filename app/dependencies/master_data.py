1f3035b53277dd5c5c559e5e81085510c5996ebc
Chirag Jain <cjain9975@gmail.com>
2026-04-30 10:20:50 +0530

    IRTC Backend System

--- app/dependencies/master_data.py
+++ app/dependencies/master_data.py
@@ -1,29 +1,28 @@
 
 from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.infrastructure.database.session import get_db
 from app.domains.master_data.services.stations_services import StationsService
 from app.domains.master_data.services.trains_services import TrainsService
 from app.domains.master_data.services.routes_services import RoutesService
 from app.domains.master_data.services.schedules_services import TrainSchedulesService
-from app.domains.master_data.repository.base import MasterDataRepositoryBase
-from app.dependencies.repositories import get_master_data_repository
-
 
 def get_stations_service(
-    repo: MasterDataRepositoryBase = Depends(get_master_data_repository),
+    db_session: AsyncSession = Depends(get_db),
 ) -> StationsService:
-    return StationsService(repo)
+    return StationsService(db_session)
 
 def get_trains_service(
-    repo: MasterDataRepositoryBase = Depends(get_master_data_repository),
+    db_session: AsyncSession = Depends(get_db),
 ) -> TrainsService:
-    return TrainsService(repo)
+    return TrainsService(db_session)
 
 def get_routes_service(
-    repo: MasterDataRepositoryBase = Depends(get_master_data_repository),
+    db_session: AsyncSession = Depends(get_db),
 ) -> RoutesService:
-    return RoutesService(repo)
+    return RoutesService(db_session)
 
 def get_train_schedules_service(
-    repo: MasterDataRepositoryBase = Depends(get_master_data_repository),
+    db_session: AsyncSession = Depends(get_db),
 ) -> TrainSchedulesService:
-    return TrainSchedulesService(repo)
+    return TrainSchedulesService(db_session)
