784a6e573772842bb2d6c9e5d51953a424e6dc37
Chirag Jain <cjain9975@gmail.com>
2026-05-01 15:56:41 +0530

    IRTC Backend System

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -7,6 +7,8 @@ from fastapi import APIRouter
 from app.api.v1.users import router as users_router
 from app.api.v1.auth import router as auth_router
 from app.api.v1.admin_master_data import router as admin_master_data_router
+from app.api.v1.search_discovery import router as search_discovery_router
+
 
 
 router = APIRouter()
@@ -27,4 +29,9 @@ router.include_router(
     admin_master_data_router,
     prefix="/admin/master-data",
     tags=["Admin Master Data"],
+)
+
+router.include_router(
+    search_discovery_router,
+    tags=["Search Discovery"],
 )
