ce1325658efde278b08b76a8f0b958c8888e01bf
Chirag Jain <cjain9975@gmail.com>
2026-05-01 17:20:21 +0530

    IRTC Backend System

--- app/middlewares/exception_middleware.py
+++ app/middlewares/exception_middleware.py
@@ -29,6 +29,7 @@ class ExceptionMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
 
         try:
+            
             response = await call_next(request)
             return response
 
