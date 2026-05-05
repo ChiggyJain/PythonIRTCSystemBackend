c2ca2adf9843cbf8c8c4fabe0099c3a3987234a2
Chirag Jain <cjain9975@gmail.com>
2026-05-05 17:51:14 +0530

    IRTC Backend System

---
+++ app/domains/booking/models/bookings_models.py
@@ -0,0 +1,56 @@
+
+from datetime import datetime
+from sqlalchemy import Integer, Numeric, DateTime, Enum, func, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+from app.common.utils.datetime import now_ist
+from app.infrastructure.database.base import Base
+
+
+class SeatInventory(Base):
+    
+    __tablename__ = "SEAT_INVENTORY"
+    __table_args__ = (
+        UniqueConstraint("schedule_id", "seat_id", name="uq_scheduleIdSeatId"),
+        UniqueConstraint("schedule_id", "seat_number", name="uq_scheduleIdSeatNumber"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    schedule_inventory_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    schedule_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    seat_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    seat_number: Mapped[int] = mapped_column(Integer, nullable=False)
+    seat_type: Mapped[str] = mapped_column(
+        Enum("LOWER", "MIDDLE", "UPPER", "SIDE_LOWER", "SIDE_UPPER", name="seat_type_enum"),
+        nullable=False,
+    )
+    price: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False, default=0, server_default="0.00")
+    locked_by_user_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    locked_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=now_ist,
+        nullable=True,
+    )
+    locked_expires_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=now_ist,
+        nullable=True,
+    )
+    booking_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    version: Mapped[int] = mapped_column(Integer, nullable=False, default=0, server_default="0")
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=now_ist,
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=now_ist,
+        onupdate=now_ist,
+        nullable=False,
+    )    
+    status: Mapped[str] = mapped_column(
+        Enum("AVAILABLE", "LOCKED", "BOOKED", "CANCELLED", name="seat_inventory_status_enum"),
+        nullable=False,
+        default="AVAILABLE",
+        server_default="AVAILABLE",
+    )
