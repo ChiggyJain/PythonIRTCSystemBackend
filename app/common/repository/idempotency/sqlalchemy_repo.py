348a2d9c37845211ebec777e8df80fb66a540ae4
Chirag Jain <cjain9975@gmail.com>
2026-05-05 19:40:32 +0530

    IRTC Backend System

--- app/common/repository/idempotency/sqlalchemy_repo.py
+++ app/common/repository/idempotency/sqlalchemy_repo.py
@@ -1,6 +1,7 @@
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Any
 from app.common.utils.datetime import now_ist
 from app.common.models.idempotencyrecord_models import IdempotencyRecords
 
@@ -17,10 +18,14 @@ class IdempotencySQLAlchemyRepository:
         return result.scalar_one_or_none()
 
 
-    async def add_idempotency_record(self, *, event_key: str, event_type: str | None = None, event_response: str | None = None) -> IdempotencyRecords:
+    async def add_idempotency_record(
+        self, *, event_key: str, event_type: str | None = None, event_response: dict[str, Any] | None = None
+    ) -> IdempotencyRecords:
+        
         row = IdempotencyRecords(
             event_key=event_key,
             event_type=event_type,
+            event_response=event_response,
             created_at=now_ist(),
             updated_at=now_ist(),
         )
