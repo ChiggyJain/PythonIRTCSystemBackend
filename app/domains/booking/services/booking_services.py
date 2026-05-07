e78659cf5557358f81502c246f13a28aeb91a385
Chirag Jain <cjain9975@gmail.com>
2026-05-07 20:20:24 +0530

    IRTC Backend System

--- app/domains/booking/services/booking_services.py
+++ app/domains/booking/services/booking_services.py
@@ -157,7 +157,6 @@ class BookingService:
             bookingId = created_booking.id            
             created_booking_seats = await self.booking_repo.create_booking_seats(booking_id=bookingId, seat_details=bookingSeats)
             created_booking_passengers = await self.booking_repo.create_booking_passengers(booking_id=bookingId, passenger_details=passengers)
-            
             created_booking_saga_logs = await self.booking_repo.create_booking_saga_logs(
                 booking_id=bookingId, 
                 saga_step="HOLD_SEATS", 
