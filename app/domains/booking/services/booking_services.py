6328c8cf5350aad9fb22496ce06ebadb4b6cb218
Chirag Jain <cjain9975@gmail.com>
2026-05-07 16:51:14 +0530

    IRTC Backend System

--- app/domains/booking/services/booking_services.py
+++ app/domains/booking/services/booking_services.py
@@ -119,12 +119,17 @@ class BookingService:
                 version=0,
                 status="PENDING",
             )
-
-            bookingId = created_booking.id
-            
+            bookingId = created_booking.id            
             created_booking_seats = await self.booking_repo.create_booking_seats(booking_id=bookingId, seat_details=bookingSeats)
-            
             created_booking_passengers = await self.booking_repo.create_booking_passengers(booking_id=bookingId, passenger_details=passengers)
+            created_booking_saga_logs = await self.booking_repo.create_booking_saga_logs(
+                booking_id=bookingId, saga_step="HOLD_SEATS", 
+                request={
+                    "user_id":user_id, "schedule_id":schedule_id, "seat_ids":seat_ids, "ttlSeconds" : settings.LOCK_TTL_SECONDS,
+                    "from_station_sequence_number":from_station_sequence_number, "to_station_sequence_number":to_station_sequence_number                     
+                },
+                response=None, error=None, status="PENDING"
+            )
 
 
         except Exception:
