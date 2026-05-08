152f5fe15a5b7dcdcfeee2214bddc3ce6bc3503e
Chirag Jain <cjain9975@gmail.com>
2026-05-08 16:25:14 +0530

    IRTC Backend System

--- app/api/v1/inventory.py
+++ app/api/v1/inventory.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, Query
 from app.common.decorators.feature_control import feature_control
 from app.common.utils.ratelimiter import rate_limiter
 from app.core.exceptions import BaseAppException
@@ -10,14 +10,9 @@ from app.domains.inventory.services.inventory_services import InventoryService
 from app.dependencies.inventory import get_inventory_service
 
 
-
 settings = get_settings()
 router = APIRouter()
 
-
-# -------------------------
-# stations create process
-# -------------------------
 
 @feature_control(
     {
@@ -41,6 +36,37 @@ async def get_inventory_schedules_availabiliity(
 router.add_api_route(
     "/schedules/{schedule_id}/availability",
     get_inventory_schedules_availabiliity,
+    methods=["GET"],
+    route_class_override=FeatureAPIRoute,
+)
+
+
+@feature_control(
+    {
+        "name": "v1.inventory.schedules.schedule_id.seats.availability",
+        "logging": {
+            "console": True,
+            "file": True,
+        },
+        "rate_limit": {
+            "limit": 1000,
+            "window": 60,
+        },
+    }
+)
+async def get_inventory_schedules_seats_availabiliity(
+    schedule_id: int,    
+    from_station_sequence_number: int = Query(...),
+    to_station_sequence_number: int = Query(...),
+    service: InventoryService = Depends(get_inventory_service),
+):
+    return await service.get_inventory_schedules_seats_availabiliity(
+        schedule_id=schedule_id, from_station_sequence_number=from_station_sequence_number, to_station_sequence_number=to_station_sequence_number
+    )
+
+router.add_api_route(
+    "/schedules/{schedule_id}/seats",
+    get_inventory_schedules_seats_availabiliity,
     methods=["GET"],
     route_class_override=FeatureAPIRoute,
 )
