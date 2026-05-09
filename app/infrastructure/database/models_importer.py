18eb1acf8b2a8605affa6072fa44a07f6a9d7b64
Chirag Jain <cjain9975@gmail.com>
2026-05-09 22:11:11 +0530

    IRTC Backend System

--- app/infrastructure/database/models_importer.py
+++ app/infrastructure/database/models_importer.py
@@ -1,20 +1,4 @@
 
-"""
-Models Importer
-This module imports all ORM models so that
-SQLAlchemy Base.metadata can detect them.
-Why needed?
------------
-In Domain Driven Design (DDD), models are spread across domains.
-Alembic autogenerate only detects tables that are imported.
-So we must import all models here.
-IMPORTANT:
-----------
-Do NOT add logic here.
-Only import models.
-"""
-
-
 from app.domains.users.models import users_models  # noqa
 from app.domains.auth.models import usertokens_models  # noqa
 from app.domains.security.models import models # noqa
