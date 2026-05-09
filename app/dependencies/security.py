27622000bef77e4625bb8e2919fb0d8090fa0772
Chirag Jain <cjain9975@gmail.com>
2026-05-09 17:59:48 +0530

    IRTC Backend System

--- app/dependencies/security.py
+++ app/dependencies/security.py
@@ -1,15 +1,10 @@
 
-"""
-Security dependencies
-"""
-
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.infrastructure.database.session import get_db
 from app.domains.security.services.passwordchanged_services import PasswordChangeOtpService
 from app.domains.security.services.emailverification_services import EmailVerificationOtpService
 from app.domains.security.services.emailchanged_services import EmailChangedOtpService
-
 
 
 def get_password_change_otp_service(
