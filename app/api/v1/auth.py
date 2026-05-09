66e6e14cbfd583fe8fc1f3689e896602018cdd2d
Chirag Jain <cjain9975@gmail.com>
2026-05-09 15:40:35 +0530

    IRTC Backend System

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -5,13 +5,10 @@ from fastapi import (
 )
 from app.core.routing.feature_route import FeatureAPIRoute
 from app.common.decorators.feature_control import feature_control
-from app.core.response import success_response
 from app.dependencies.auth import (
     get_auth_service, 
-    get_token_service,
 )
 from app.domains.auth.services.auth_services import AuthService
-from app.domains.auth.services.token_services import TokenService
 from app.domains.auth.schemas.schemas import (
     RefreshTokenRequest,
     LogoutRequest,
@@ -19,14 +16,8 @@ from app.domains.auth.schemas.schemas import (
 from app.core.exceptions import BaseAppException
 from app.common.security.token_decoder import(
     get_current_user_details_from_access_token,
-    get_current_user_details_from_refresh_token
 )
-from app.common.cache.redis_cache import (
-    build_cache_key,
-    cache_delete,
-    build_cache_set_key,
-    cache_set_remove
-)
+
 
 
 router = APIRouter()
