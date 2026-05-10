7c3d83d1d071c1a68adab4423db6107e955050c3
Chirag Jain <cjain9975@gmail.com>
2026-05-10 17:34:16 +0530

    IRTC Backend System

---
+++ app/domains/payments/schemas/payments_schemas.py
@@ -0,0 +1,35 @@
+
+import re
+from typing import List, Literal
+from pydantic import (
+    BaseModel,
+    field_validator,
+    model_validator,
+    ValidationInfo
+)
+
+
+
+class CreatePaymentOrderRequest(BaseModel):
+
+    user_id: int
+    idempotency_key: str
+    booking_id: int
+    amount: int
+    
+    @field_validator(
+        "user_id",
+        "booking_id",
+        "amount",
+    )
+    @classmethod
+    def validate_positive_integer_fields(
+        cls,
+        value: int,
+        info: ValidationInfo,
+    ):
+        if value <= 0:
+            raise ValueError(
+                f"{info.field_name} must be greater than 0."
+            )
+        return value
