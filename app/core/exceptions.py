8ada04469ef7392373a6a1584b870b8b4c0c1f18
Chirag Jain <cjain9975@gmail.com>
2026-05-10 08:47:09 +0530

    IRTC Backend System

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,6 +1,6 @@
 
 from typing import (
-    List, Optional
+    List, Optional, Any
 )
 
 class BaseAppException(Exception):
@@ -8,85 +8,10 @@ class BaseAppException(Exception):
         self,
         status_code: int,
         messages: List[str],
-        data: Optional[object] = None,
+        data: Optional[Any] = None,
     ):
         self.status_code = status_code
         self.messages = messages
         self.data = data
 
 
-class BadRequestException(BaseAppException):
-    def __init__(
-        self,
-        messages: List[str] = ["Bad Request"],
-        data: object = None,
-    ):
-        super().__init__(
-            status_code=400,
-            messages=messages,
-            data=data,
-        )
-
-
-class NotFoundException(BaseAppException):
-    def __init__(
-        self,
-        messages: List[str] = ["Not Found"],
-        data: object = None,
-    ):
-        super().__init__(
-            status_code=404,
-            messages=messages,
-            data=data,
-        )
-
-
-class UnauthorizedException(BaseAppException):
-    def __init__(
-        self,
-        messages: List[str] = ["Unauthorized"],
-        data: object = None,
-    ):
-        super().__init__(
-            status_code=401,
-            messages=messages,
-            data=data,
-        )
-
-
-class ForbiddenException(BaseAppException):
-    def __init__(
-        self,
-        messages: List[str] = ["Forbidden"],
-        data: object = None,
-    ):
-        super().__init__(
-            status_code=403,
-            messages=messages,
-            data=data,
-        )
-
-class ConflictException(BaseAppException):
-    def __init__(
-        self,
-        messages: List[str] = ["Conflict"],
-        data: object = None,
-    ):
-        super().__init__(
-            status_code=409,
-            messages=messages,
-            data=data,
-        )
-
-
-class InternalServerException(BaseAppException):
-    def __init__(
-        self,
-        messages: List[str] = ["Internal Server Error"],
-        data: object = None,
-    ):
-        super().__init__(
-            status_code=500,
-            messages=messages,
-            data=data,
-        )
