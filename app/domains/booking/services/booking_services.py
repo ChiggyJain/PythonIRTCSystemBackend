bc50d0aea132dd829dd90ce78462bf4b6766ebcb
Chirag Jain <cjain9975@gmail.com>
2026-05-10 10:17:59 +0530

    IRTC Backend System

--- app/domains/booking/services/booking_services.py
+++ app/domains/booking/services/booking_services.py
@@ -4,8 +4,10 @@ import json
 import httpx
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.common.utils.logger import app_logger
 from app.core.exceptions import BaseAppException
+from app.core.response import (
+    standardize_response, 
+)
 from app.core.settings import get_settings
 from app.common.utils.datetime import now_ist, today_ist
 from app.common.utils.orm_to_dict import orm_to_dict
@@ -25,6 +27,7 @@ from app.services.saga_services import (
 settings = get_settings()
 
 
+
 class BookingService:
 
     def __init__(self, db_session: AsyncSession):
@@ -36,114 +39,115 @@ class BookingService:
 
     async def create_booking_details(self, *, payload: dict) -> dict:
         
-        # extracted parameters
-        user_id = int(payload.get("user_id", 0))
-        idempotency_key = payload.get("idempotency_key", "")
-        schedule_id = int(payload.get("schedule_id", 0))
-        from_station_id = int(payload.get("from_station_id", 0))
-        to_station_id = int(payload.get("to_station_id", 0))
-        from_station_sequence_number = int(payload.get("from_station_sequence_number", 0))
-        to_station_sequence_number = int(payload.get("to_station_sequence_number", 0))
-        seat_ids = payload.get("seat_ids", [])
-        seat_ids.sort()
-        passengers = payload.get("passengers", [])
-        IDEMPOTENCY_EVENT_TYPE = "booking"
-        IDEMPOTENCY_EVENT_KEY_PREFIX = "booking"
-
-
-        # checking given idempotency key exists or not
-        event_key = f"{IDEMPOTENCY_EVENT_KEY_PREFIX}:{idempotency_key}"
-        existing_idempotency_record = await self.idempotency_repo.get_idempotency_record_by_event_key(event_key)
-        if existing_idempotency_record:
-            return existing_idempotency_record.event_response
-        
-        # fetching schedule-inventory details
-        inventoryScheduleDataObj = None
-        async with httpx.AsyncClient() as client:
-            response = await client.get(f"http://127.0.0.1:8000/inventory/schedules/{schedule_id}/availability")
-            response.raise_for_status()
-            data = response.json()
-            inventoryScheduleDataObj = data.get("data", None)
-        if inventoryScheduleDataObj == None:
-            raise BaseAppException(
-                status_code=400,
-                messages=[f"No inventory schedule found for Train-Schedule-ID: {schedule_id}"],
-            )
-        if inventoryScheduleDataObj["status"]!="ACTIVE":
-            raise BaseAppException(
-                status_code=400,
-                messages=[f"Inventory schedule is not active for Train-Schedule-ID: {schedule_id}"],
-            )
-        if inventoryScheduleDataObj["departure_date"]<today_ist():
-            raise BaseAppException(
-                status_code=400,
-                messages=[f"Train is already departed for Train-Schedule-ID: {schedule_id}"],
-            )
-        
-        # fetching seats details
-        seatData = None
-        async with httpx.AsyncClient() as client:
-            response = await client.get(f"http://127.0.0.1:8000/inventory/schedules/{schedule_id}/seats", params={
-                "from_station_sequence_number" : from_station_sequence_number,
-                "to_station_sequence_number" : to_station_sequence_number
-            })
-            response.raise_for_status()
-            data = response.json()
-            seatData = data.get("data", None)
-        if seatData == None:
-            raise BaseAppException(
-                status_code=400,
-                messages=[f"Seats details is not found for Train-Schedule-ID: {schedule_id}"],
-            )
-
-        # calculating seat price and availability details
-        seatMap = {eachSeatDataObj["seat_id"] : eachSeatDataObj for eachSeatDataObj in seatData.seats}
-        bookingSeats = []
-        totalAmount = 0
-        for eachGivenSeatId in seat_ids:
-            seat = seatMap.get(eachGivenSeatId, None)
-            if seat == None:
-                raise BaseAppException(
-                    status_code=400,
-                    messages=[f"Given Seat-ID: {eachGivenSeatId} is not in inventory schedule"],
-                )
-            is_seat_available = (
-                seat["segment_status"] == "AVAILABLE"
-                if (
-                    from_station_sequence_number
-                    and to_station_sequence_number
-                    and seat.get("segment_status") is not None
-                )
-                else seat["status"] == "AVAILABLE"
-            )
-            if is_seat_available == False:
-                raise BaseAppException(
-                    status_code=400,
-                    messages=[f"Seat: {seat["seat_number"]} is not available"],
-                )
-            if is_seat_available == True:
-                bookingSeats.append(seat)
-                totalAmount+= seat["price"]
-
-   
-        # preparing keys to acquire seat locks in redis via lua_script
-        allRedisKeys = []
-        for eachSeatId in seat_ids:
-            key = f"booking:lock:seat:{schedule_id}:{eachSeatId}:{from_station_sequence_number}:{to_station_sequence_number}"
-            allRedisKeys.append(key)
-        curTimeStamp = int(datetime.now().timestamp())    
-        redisKeyValue = f"pre-{curTimeStamp}:{curTimeStamp}"
-        acquiredSeatLocksResponse = await acquireBookingSeatLocksThroughRedis(allRedisKeys, redisKeyValue, settings.BOOKING_TTL_SECONDS)
-        if acquiredSeatLocksResponse.isSuccess == False:
-            raise BaseAppException(
-                status_code=400,
-                messages=[f"One or more seats are being booked by another user. Please try again"],
-            )
-
-        booking_details = {}
-
         try:
+
+            # extracted parameters
+            user_id = int(payload.get("user_id", 0))
+            idempotency_key = payload.get("idempotency_key", "")
+            schedule_id = int(payload.get("schedule_id", 0))
+            from_station_id = int(payload.get("from_station_id", 0))
+            to_station_id = int(payload.get("to_station_id", 0))
+            from_station_sequence_number = int(payload.get("from_station_sequence_number", 0))
+            to_station_sequence_number = int(payload.get("to_station_sequence_number", 0))
+            seat_ids = payload.get("seat_ids", [])
+            seat_ids.sort()
+            passengers = payload.get("passengers", [])
+            IDEMPOTENCY_EVENT_TYPE = "booking"
+            IDEMPOTENCY_EVENT_KEY_PREFIX = "booking"
+
+
+            # checking given idempotency key exists or not
+            event_key = f"{IDEMPOTENCY_EVENT_KEY_PREFIX}:{idempotency_key}"
+            existing_idempotency_record = await self.idempotency_repo.get_idempotency_record_by_event_key(event_key)
+            if existing_idempotency_record:
+                return existing_idempotency_record.event_response
             
+            # fetching schedule-inventory details
+            inventoryScheduleDataObj = None
+            async with httpx.AsyncClient() as client:
+                response = await client.get(f"http://127.0.0.1:8000/inventory/schedules/{schedule_id}/availability")
+                response.raise_for_status()
+                data = response.json()
+                inventoryScheduleDataObj = data.get("data", None)
+            if inventoryScheduleDataObj == None:
+                raise BaseAppException(
+                    status_code=400,
+                    messages=[f"No inventory schedule found for Train-Schedule-ID: {schedule_id}"],
+                )
+            if inventoryScheduleDataObj["status"]!="ACTIVE":
+                raise BaseAppException(
+                    status_code=400,
+                    messages=[f"Inventory schedule is not active for Train-Schedule-ID: {schedule_id}"],
+                )
+            if inventoryScheduleDataObj["departure_date"]<today_ist():
+                raise BaseAppException(
+                    status_code=400,
+                    messages=[f"Train is already departed for Train-Schedule-ID: {schedule_id}"],
+                )
+            
+            # fetching seats details
+            seatData = None
+            async with httpx.AsyncClient() as client:
+                response = await client.get(f"http://127.0.0.1:8000/inventory/schedules/{schedule_id}/seats", params={
+                    "from_station_sequence_number" : from_station_sequence_number,
+                    "to_station_sequence_number" : to_station_sequence_number
+                })
+                response.raise_for_status()
+                data = response.json()
+                seatData = data.get("data", None)
+            if seatData == None:
+                raise BaseAppException(
+                    status_code=400,
+                    messages=[f"Seats details is not found for Train-Schedule-ID: {schedule_id}"],
+                )
+
+            # calculating seat price and availability details
+            seatMap = {eachSeatDataObj["seat_id"] : eachSeatDataObj for eachSeatDataObj in seatData.seats}
+            bookingSeats = []
+            totalAmount = 0
+            for eachGivenSeatId in seat_ids:
+                seat = seatMap.get(eachGivenSeatId, None)
+                if seat == None:
+                    raise BaseAppException(
+                        status_code=400,
+                        messages=[f"Given Seat-ID: {eachGivenSeatId} is not in inventory schedule"],
+                    )
+                is_seat_available = (
+                    seat["segment_status"] == "AVAILABLE"
+                    if (
+                        from_station_sequence_number
+                        and to_station_sequence_number
+                        and seat.get("segment_status") is not None
+                    )
+                    else seat["status"] == "AVAILABLE"
+                )
+                if is_seat_available == False:
+                    raise BaseAppException(
+                        status_code=400,
+                        messages=[f"Seat: {seat["seat_number"]} is not available"],
+                    )
+                if is_seat_available == True:
+                    bookingSeats.append(seat)
+                    totalAmount+= seat["price"]
+
+    
+            # preparing keys to acquire seat locks in redis via lua_script
+            allRedisKeys = []
+            for eachSeatId in seat_ids:
+                key = f"booking:lock:seat:{schedule_id}:{eachSeatId}:{from_station_sequence_number}:{to_station_sequence_number}"
+                allRedisKeys.append(key)
+            curTimeStamp = int(datetime.now().timestamp())    
+            redisKeyValue = f"pre-{curTimeStamp}:{curTimeStamp}"
+            acquiredSeatLocksResponse = await acquireBookingSeatLocksThroughRedis(allRedisKeys, redisKeyValue, settings.BOOKING_TTL_SECONDS)
+            if acquiredSeatLocksResponse.isSuccess == False:
+                raise BaseAppException(
+                    status_code=400,
+                    messages=[f"One or more seats are being booked by another user. Please try again"],
+                )
+
+            booking_details = {}
+            
+
             curDateTime = now_ist()
             dt = datetime.strptime(curDateTime, "%Y-%m-%d %H:%M:%S")
             locked_expires_at = dt + timedelta(seconds=settings.LOCK_TTL_SECONDS)
