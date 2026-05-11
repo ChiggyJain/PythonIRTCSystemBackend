336b210a0147c0ad846c4167c6269ba942baa1e2
Chirag Jain <cjain9975@gmail.com>
2026-05-11 17:43:53 +0530

    IRTC Backend System

--- app/domains/payments/services/payment_services.py
+++ app/domains/payments/services/payment_services.py
@@ -246,6 +246,93 @@ class PaymentService:
                 }
                 payment_gateway_order_refund_rsp_obj = await payment_gateway_class_instances_obj.initiateRefund(**params2)
             
+                # failed due to some reasons
+                if payment_gateway_order_refund_rsp_obj["status_code"]!=200:
+
+                    # storing idempotency-key details
+                    await self.idempotency_repo.add_idempotency_record(
+                        event_key = event_key,
+                        event_type = "payment_orders",
+                        event_response = {
+                            "status_code" : payment_gateway_order_refund_rsp_obj["status_code"],
+                            "messages" : payment_gateway_order_refund_rsp_obj["messages"],
+                            "payment_order_id" : "",
+                            "gateway_provider" : "",
+                            "gateway_provider_key_id" : "",
+                            "gateway_order_id" : "",
+                            "amount" : "",
+                            "currency" : "",
+                            "payment_order_status" : ""
+                        }
+                    )
+
+                    await self._db_session.commit()
+                    
+                    return standardize_response(
+                        status_code=500,
+                        messages=payment_gateway_order_refund_rsp_obj["messages"]
+                    )
+                
+                # successfully created
+                if payment_gateway_order_refund_rsp_obj["status_code"] == 200:
+                    
+                    # creating payment orders into table
+                    created_payment_orders_row = await self.payment_repo.create_refund_orders(
+                        idempotency_key=idempotency_key,
+                        payment_order_id=payment_order.id,
+                        total_amount=amount,
+                        reason=reason,
+                        gateway_refund_id=payment_gateway_order_refund_rsp_obj["payment_gateway_refund_id"],
+                        failure_reason=None,
+                        metadata_json=None,
+                        status="INITIATED"
+                    )
+
+                    # creating payment audit logs into table
+                    created_payment_audit_logs_row = await self.payment_repo.create_payment_audit_logs(
+                        payment_order_id=payment_order.id
+                        action="REFUND_INITIATED",
+                        gateway_response=payment_gateway_order_refund_rsp_obj["raw_response"],
+                        metadata_json={
+                            "refund_id" : payment_gateway_order_refund_rsp_obj["payment_gateway_refund_id"],
+                            "amount" : amount,
+                            "reason" : reason
+                        },
+                        status="A"
+                    )
+                    
+                    # storing idempotency-key details
+                    await self.idempotency_repo.add_idempotency_record(
+                        event_key = event_key,
+                        event_type = "payment_orders",
+                        event_response = {
+                            "status_code" : 201,
+                            "messages" : ["Payment order refund created successfully"],
+                            "payment_order_id" : created_payment_orders_row.id,
+                            "gateway_provider" : created_payment_orders_row.gateway_provider,
+                            "gateway_provider_key_id" : "",
+                            "gateway_order_id" : created_payment_orders_row.gateway_order_id,
+                            "amount" : created_payment_orders_row.total_amount,
+                            "currency" : created_payment_orders_row.currency,
+                            "payment_order_status" : created_payment_orders_row.status
+                        }
+                    )
+
+                    await self._db_session.commit()
+
+                    return standardize_response(
+                        status_code=201,
+                        messages=[f"Payment orders created successfully"],
+                        data={
+                            "payment_order_id" : created_payment_orders_row.id,
+                            "gateway_provider" : created_payment_orders_row.gateway_provider,
+                            "gateway_provider_key_id" : "",
+                            "gateway_order_id" : created_payment_orders_row.gateway_order_id,
+                            "amount" : created_payment_orders_row.total_amount,
+                            "currency" : created_payment_orders_row.currency,
+                            "payment_order_status" : created_payment_orders_row.status
+                        }
+                    )
 
     
         except Exception as e:
