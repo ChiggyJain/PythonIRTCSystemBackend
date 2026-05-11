953dd92580102adc34a9b838d8bee90fe8d3cf53
Chirag Jain <cjain9975@gmail.com>
2026-05-11 11:59:31 +0530

    IRTC Backend System

--- app/domains/inventory/models/schedule_inventory_models.py
+++ app/domains/inventory/models/schedule_inventory_models.py
@@ -8,6 +8,8 @@ from app.infrastructure.database.base import Base
 
 class ScheduleInventory(Base):
 
+    __slots__ = ()
+
     __tablename__ = "SCHEDULE_INVENTORY"
     __table_args__ = (
         UniqueConstraint("schedule_id", name="uq_scheduledId"),
