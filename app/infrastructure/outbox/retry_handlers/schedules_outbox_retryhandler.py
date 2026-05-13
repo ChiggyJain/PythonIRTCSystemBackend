8d52373310d15aee32d932308e328ac19ff19ab7
Chirag Jain <cjain9975@gmail.com>
2026-05-13 14:18:37 +0530

    IRTC Backend System

--- app/infrastructure/outbox/retry_handlers/schedules_outbox_retryhandler.py
+++ app/infrastructure/outbox/retry_handlers/schedules_outbox_retryhandler.py
@@ -20,7 +20,7 @@ class MasterDataSchedulesOutboxRetryHandler(OutboxBaseRetryHandler):
 
         now = now_ist()
         retry_count_after = int(event.retry_count) + 1
-        max_retries = int(settings.KAFKA_SCHEDULE_CREATED_TOPIC)
+        max_retries = int(settings.SCHEDULE_OUTBOX_MAX_RETRIES)
 
         if retry_count_after>=max_retries:
             await self.outbox_repo.mark_outbox_failed(
