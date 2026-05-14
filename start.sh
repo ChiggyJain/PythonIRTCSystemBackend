400db3fefd2cf0222a446d346998f8e7ad337709
Chirag Jain <cjain9975@gmail.com>
2026-05-14 20:14:46 +0530

    IRTC Backend System

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ set -e
 
 printf "Waiting for MySQL server to start...\n"
 until nc -z mysql 3306; do
-  printf "MySQL server not ready..."
+  printf "MySQL server not ready...\n"
   sleep 2
 done
 printf "MySQL server started...\n"
 
 printf "Waiting for Redis server to start...\n"
 until nc -z redis 6379; do
-    printf "Redis server not ready..."
+    printf "Redis server not ready...\n"
     sleep 2
 done
 printf "Redis server started...\n"
@@ -19,7 +19,7 @@ printf "Redis server started...\n"
 
 printf "Waiting for Kafka server to start...\n"
 until nc -z kafka 9092; do
-  printf "Kafka server not ready..."
+  printf "Kafka server not ready...\n"
   sleep 2
 done
 printf "Kafka server started...\n"
@@ -27,7 +27,7 @@ printf "Kafka server started...\n"
 
 printf "Waiting for Elasticsearch server to start...\n"
 until nc -z elasticsearch 9200; do
-  printf "Elasticsearch server not ready..."
+  printf "Elasticsearch server not ready...\n"
   sleep 2
 done
 printf "Elasticsearch server started...\n"
@@ -39,6 +39,6 @@ printf "Applying Alembic migration...\n"
 alembic upgrade head
 printf "Alembic migration completed...\n"
 
-printf "Alembic migration completed...\n"
+printf "FastAPI server to start...\n"
 uvicorn app.main:app --host 0.0.0.0 --port 8000
 printf "FastAPI server started...\n"
