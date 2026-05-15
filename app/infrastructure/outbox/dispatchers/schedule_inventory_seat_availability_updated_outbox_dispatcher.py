2e0da544e45633cbc36ab944e34a55c1c939daed
Chirag Jain <cjain9975@gmail.com>
2026-05-15 16:55:15 +0530

    IRTC Backend System

--- app/infrastructure/outbox/dispatchers/schedule_inventory_seat_availability_updated_outbox_dispatcher.py
+++ app/infrastructure/outbox/dispatchers/schedule_inventory_seat_availability_updated_outbox_dispatcher.py
@@ -1,6 +1,7 @@
 
 import asyncio
 import json
+import signal
 from app.common.utils.logger import app_logger
 from app.core.settings import get_settings
 from app.infrastructure.kafka.client import build_consumer
@@ -12,6 +13,14 @@ from app.infrastructure.elasticsearch.repositories.routes_repository import (
 )
 
 settings = get_settings()
+shutdown_event = asyncio.Event()
+
+def shutdown_handler():
+    app_logger.info(f"Shutdown signal received")
+    shutdown_event.set()
+
+
+
 
 
 async def update_schedule_inventory_to_elasticsearch(
@@ -86,27 +95,32 @@ async def run_worker():
 
     try:
 
-        async for message in consumer:
-
+        while not shutdown_event.is_set():
             try:
-
-                payload = json.loads(message.value.decode("utf-8"))
-                success = (
-                    await update_schedule_inventory_to_elasticsearch(
-                        routes_repo=routes_repo,
-                        payload=payload
-                    )
+                message_batch = await asyncio.wait_for(
+                    consumer.getmany(timeout_ms=1000),
+                    timeout=2
                 )
-                if success:
-                    await consumer.commit()
-                    app_logger.info(
-                        f"Successfully updated schedule inventory into index routes document for schedule_id: {payload.get('schedule_id')}"
-                    )
-                else:
-                    app_logger.error(
-                        f"Failed to update schedule inventory into index routes document for schedule_id: {payload.get('schedule_id')}"
-                    )
-
+                for tp, messages in message_batch.items():
+                    for message in messages:
+                        payload = json.loads(message.value.decode("utf-8"))
+                        success = (
+                            await update_schedule_inventory_to_elasticsearch(
+                                routes_repo=routes_repo,
+                                payload=payload
+                            )
+                        )
+                        if success:
+                            await consumer.commit()
+                            app_logger.info(
+                                f"Successfully updated schedule inventory into index routes document for schedule_id: {payload.get('schedule_id')}"
+                            )
+                        else:
+                            app_logger.error(
+                                f"Failed to update schedule inventory into index routes document for schedule_id: {payload.get('schedule_id')}"
+                            )
+            except asyncio.TimeoutError:
+                continue    
             except Exception as exc:
                 app_logger.exception(
                     f"Schedule inventory worker processing error: "
@@ -116,12 +130,29 @@ async def run_worker():
 
     finally:
         app_logger.info(
-            "Close schedule inventory seat availability updated consumer and elasticsearch client"
+            "Gracefully shutting down"
         )
         await consumer.stop()
         await es_client_instances.close()
+        app_logger.info(
+            "Shutdown completed"
+        )
+
+
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+    loop.add_signal_handler(
+        signal.SIGINT,
+        shutdown_handler
+    )
+    loop.add_signal_handler(
+        signal.SIGTERM,
+        shutdown_handler
+    )
+    await run_worker()
 
 
 if __name__ == "__main__":
-
-    asyncio.run(run_worker())
+    asyncio.run(main())
