397a6c5c71712490cfb67894f20df4d82977616d
Chirag Jain <cjain9975@gmail.com>
2026-05-15 20:19:24 +0530

    IRTC Backend System

--- app/workers/outbox/emailverification_otp_outbox_worker.py
+++ app/workers/outbox/emailverification_otp_outbox_worker.py
@@ -1,127 +1,192 @@
 
 import json
 import asyncio
+import signal
 from app.infrastructure.database.session import AsyncSessionLocal
 from app.common.utils.logger import app_logger
 from app.core.settings import get_settings
 from app.common.utils.datetime import now_ist
 from app.infrastructure.kafka.client import build_producer
-from app.domains.security.repository.sqlalchemy_repo import SecuritySQLAlchemyRepository
 from app.infrastructure.outbox.repository.sqlalchemy_repo import OutboxEventsSQLAlchemyRepository
 from app.infrastructure.outbox.retry_handlers.outbox_retryhandler_factory import OutboxRetryHandlerFactory
 
 
 settings = get_settings()
+shutdown_event = asyncio.Event()
+
 POLL_INTERVAL_IDLE_SECONDS = 2
 POLL_INTERVAL_ACTIVE_SECONDS = 0.2
 BATCH_SIZE = 100
 
+def shutdown_handler():
+    app_logger.info(
+        "Shutdown signal received"
+    )
+    shutdown_event.set()
+
+
+
+async def process_event(
+    producer,
+    event
+):
+
+    payload = event.payload_json or {}
+    topic = settings.KAFKA_EMAILVERIFICATION_OTP_TOPIC
+    
+    message = json.dumps(
+        {
+            "outbox_id": event.id,
+            "event_type": event.event_type,
+            **payload
+        },
+        separators=(",", ":"),
+    ).encode("utf-8")
+
+    key = str(
+        payload.get("user_id", 0)
+    ).encode("utf-8")
+
+    await producer.send_and_wait(
+        topic=topic,
+        key=key,
+        value=message
+    )
+
+
 
 async def run_worker() -> None:
 
-    producer = build_producer(client_id=f"{settings.KAFKA_CLIENT_ID}-emailverification-outbox-publisher")
+    producer = build_producer(
+        client_id=f"{settings.KAFKA_CLIENT_ID}-emailverification-outbox-publisher"
+    )
     await producer.start()
     app_logger.info("emailverification_otp_outbox_worker started")
 
     try:
 
-        while True:
-            
+        while not shutdown_event.is_set():
+
             processed = False
 
-            for loopNo in range(1, BATCH_SIZE):
-
-                # STEP1: LOCK + MARK PROCESSING
-                async with AsyncSessionLocal() as db:
-                    async with db.begin():
-                        # fetching outbox_event repository class object
-                        outbox_repo = OutboxEventsSQLAlchemyRepository(db)
-                        # fetching pending/retry/ outbox event details only
-                        events = await outbox_repo.fetch_pending_outbox_events(
-                            event_type="EMAILVERIFICATION_OTP_DISPATCH_REQUESTED", limit=1, now_time=now_ist(),
+            # Step1: Fetching queries in batches for LOCK + MARK PROCESSING
+            async with AsyncSessionLocal() as db:
+                async with db.begin():
+                    outbox_repo = OutboxEventsSQLAlchemyRepository(db)
+                    events = await outbox_repo.fetch_pending_outbox_events(
+                        aggregate_type="OTP_CHALLENGE", 
+                        event_type="PWDCHANGED_OTP_REQUESTED",
+                        limit=BATCH_SIZE, 
+                        now_time=now_ist(),
+                    )
+                    if not events:
+                        await asyncio.sleep(
+                            POLL_INTERVAL_IDLE_SECONDS
                         )
-                        if not events:
-                            break
-                        event = events[0]
-                        payload = event.payload_json or {}
-                        user_id = int(payload.get("user_id") or 0)
-                        # updating outbox_events status as processing
-                        updated_at = now_ist()
-                        await outbox_repo.mark_outbox_processing(event=event, updated_at=updated_at)
-                
-                # STEP2: Kafka (OUTSIDE transaction)
+                        continue
+                    event_ids = [event.id for event in events]
+                    updated_at = now_ist()
+                    await outbox_repo.bulk_mark_processing(
+                        event_ids=event_ids,
+                        updated_at=updated_at
+                    )
+            
+            # Step2: Processing events outside db transactions
+            for event in events:
+
+                if shutdown_event.is_set():
+                    app_logger.info(
+                        "Shutdown detected. "
+                        "Stopping new processing."
+                    )
+                    break
+
                 try:
+
+                    await process_event(
+                        producer=producer,
+                        event=event
+                    )
+
+                    # Step3: Mark published
+                    async with AsyncSessionLocal() as db:
+                        async with db.begin():
+                            outbox_repo = OutboxEventsSQLAlchemyRepository(db)
+                            db_event = await outbox_repo.get_by_id(event.id)
+                            if db_event:
+                                await outbox_repo.mark_outbox_published(
+                                    event=db_event,
+                                    published_at=now_ist()
+                                )
                     
-                    # kafka topic
-                    topic = settings.KAFKA_EMAILVERIFICATION_OTP_TOPIC
-                    # preparing message for publishing to the kafka topic
-                    message = json.dumps(
-                        {"outbox_id": event.id, "event_type": event.event_type, **payload},
-                        separators=(",", ":"),
-                    ).encode("utf-8")
+                    processed = True
+                    app_logger.info(
+                        f"Published outbox_id="
+                        f"{event.id}"
+                    )
+                
+                except asyncio.CancelledError:
+                    app_logger.warning(
+                        "Worker cancelled"
+                    )
+                    raise
 
-                    # preparing key to used for publishing to the topic partition
-                    key = str(user_id if user_id > 0 else 0).encode("utf-8")
-
-                    # publish message to kafka topic
-                    md = await producer.send_and_wait(topic=topic, key=key, value=message,)
-
-                    # STEP 3: FINAL SUCCESS UPDATE
-                    async with AsyncSessionLocal() as db:
-                        async with db.begin():
-                            outbox_repo = OutboxEventsSQLAlchemyRepository(db)
-                            security_repo = SecuritySQLAlchemyRepository(db)
-                            # fetching outbox event by given ID
-                            event = await outbox_repo.get_by_id(event.id)
-                            # updating outbox event status as published
-                            published_at = now_ist()
-                            await outbox_repo.mark_outbox_published(event=event, published_at=published_at)
-                            # adding logs into security_event
-                            await security_repo.add_security_event(
-                                user_id=user_id,
-                                event_name="email_verification_outbox_published",
-                                event_category="OUTBOX",
-                                channel="EMAIL",
-                                provider="KAFKA",
-                                status="published",
-                                reason_code=None,
-                                correlation_id=None,
-                                request_id=None,
-                                ip_address=None,
-                                user_agent=None,
-                                metadata_json={
-                                    "outbox_id": event.id,
-                                    "topic": md.topic,
-                                    "partition": md.partition,
-                                    "offset": md.offset,
-                                },
-                            )
-                 
                 except Exception as exc:
-                    # STEP4: RETRY / FAIL
+                    
+                    app_logger.exception(
+                        f"Failed publishing "
+                        f"outbox_id={event.id}, "
+                        f"error={exc}"
+                    )
+
+                    # Step4: Retry/Fail
                     async with AsyncSessionLocal() as db:
                         async with db.begin():
                             outbox_repo = OutboxEventsSQLAlchemyRepository(db)
-                            security_repo = SecuritySQLAlchemyRepository(db)
-                            event = await outbox_repo.get_by_id(event.id)
-                            if event!=None:
-                                params = {
-                                    "retry_handler_type": "EMAILVERIFICATION_OTP", "outbox_repo": outbox_repo, "security_repo": security_repo
-                                }
-                                outbox_retry_handler_class_obj = OutboxRetryHandlerFactory.getOutboxRetryHandler(**params)
-                                params = {
-                                    "event": event, "user_id": user_id, "error_message": str(exc)
-                                }
-                                await outbox_retry_handler_class_obj.handle(**params)
+                            db_event = await outbox_repo.get_by_id(event.id)
+                            if db_event:
+                                retry_handler = OutboxRetryHandlerFactory.getOutboxRetryHandler(
+                                    retry_handler_type="EMAILVERIFICATION_OTP",
+                                    outbox_repo=outbox_repo
+                                )
+                                await retry_handler.handle(
+                                    event=db_event,
+                                    error_message=str(exc)
+                                )
 
-
-                processed = True
-
-            await asyncio.sleep(POLL_INTERVAL_ACTIVE_SECONDS if processed else POLL_INTERVAL_IDLE_SECONDS)
+                
+                await asyncio.sleep(
+                    POLL_INTERVAL_ACTIVE_SECONDS
+                    if processed
+                    else POLL_INTERVAL_IDLE_SECONDS
+                )
 
     finally:
+        app_logger.info(
+            "Flushing Kafka producer"
+        )
+        await producer.flush()
+        app_logger.info(
+            "Stopping Kafka producer"
+        )
         await producer.stop()
+        app_logger.info(
+            "Outbox worker shutdown completed"
+        )
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+    loop.add_signal_handler(
+        signal.SIGINT,
+        shutdown_handler
+    )
+    loop.add_signal_handler(
+        signal.SIGTERM,
+        shutdown_handler
+    )
+    await run_worker()
 
 
 if __name__ == "__main__":
-    asyncio.run(run_worker())
+    asyncio.run(main())
